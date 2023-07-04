867ab5b0243c4ded6f23278b93abb99dbdc06900
root <f.ibyiza@alustudent.com>
2023-07-04 07:57:21 -0700

    take 5

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+        str=[str.index("object-oriented programming"):].split()[0] + " with Python,\n" + str
+        print(str)
